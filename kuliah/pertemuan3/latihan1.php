55a11bbfb5972d1a2901f23dc66c9473adc6fecc
cassanova12 <rajabiakbaralif@gmail.com>
2022-02-26 13:05:12 +0700

    Menambahkan sintak menggunakan while

--- kuliah/pertemuan3/latihan1.php
+++ kuliah/pertemuan3/latihan1.php
@@ -5,7 +5,13 @@
 // 3.do..while
 // 4.foreach : (Pengulangan Khusus array)
 
-for( $i = 0; $i < 10; $i++ ) {
+// for( $i = 0; $i < 10; $i++ ) {
+//    echo "Hallo Dunia! <br>";
+// }
+
+$i = 0;
+while( $i < 10 ) {
     echo "Hallo Dunia! <br>";
+$i++;
 }
 ?>
